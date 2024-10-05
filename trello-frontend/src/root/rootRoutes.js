a8e20f3e1bd3efe408d3898eb237e3675c59851d
lucasgoncalvez <goncalvezlucas281@gmail.com>
2024-10-05 18:58:14 -0400

    feat: se creo la pantalla para crud de usuarios

--- trello-frontend/src/root/rootRoutes.js
+++ trello-frontend/src/root/rootRoutes.js
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { tableroRoutes } from "../modules/tablero/handlers/routes";
 import { espacioTrabajoRoutes } from "../modules/espacioTrabajo/handlers/routes";
+import { miembrosRoutes } from "../modules/miembro/handlers/routes";
 
-const routes = [...tableroRoutes, ...espacioTrabajoRoutes];
+const routes = [...tableroRoutes, ...espacioTrabajoRoutes, ...miembrosRoutes];
 
 export const RootRoutes = () => {
   return (
