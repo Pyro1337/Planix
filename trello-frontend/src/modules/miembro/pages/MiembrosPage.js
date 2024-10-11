0f17065f64d014256bc154cfda9b5ba53c213589
lucasgoncalvez <goncalvezlucas281@gmail.com>
2024-10-11 19:47:26 -0300

    feat: se agregó redux para miembros

--- trello-frontend/src/modules/miembro/pages/MiembrosPage.js
+++ trello-frontend/src/modules/miembro/pages/MiembrosPage.js
@@ -4,60 +4,10 @@ import TablaUsuarios from "../components/DataTable";
 import { useState } from "react";
 import Modal from "react-modal";
 import { SuccessToast } from "../../common/components/ToastMessage";
-
-const usuarios = [
-  {
-    nombre: "Lucas Goncalvez",
-    username: "@lucasgoncalvez",
-    actividad: "Última actividad: August 2024",
-    tableros: 2,
-    estado: "Administrador",
-  },
-  {
-    nombre: "Ivan Sánchez",
-    username: "@ivansanlafuria",
-    actividad: "No hay actividad reciente",
-    tableros: 0,
-    estado: "Pendiente",
-  },
-  {
-    nombre: "Eric Amarilla",
-    username: "@ericamarilla",
-    actividad: "No hay actividad reciente",
-    tableros: 1,
-    estado: "Colaborador",
-  },
-  {
-    nombre: "María Estefanía Aranda",
-    username: "@mariaaranda",
-    actividad: "No hay actividad reciente",
-    tableros: 1,
-    estado: "Colaborador",
-  },
-  {
-    nombre: "Julián Esteban Cerdá",
-    username: "@juliancerda",
-    actividad: "No hay actividad reciente",
-    tableros: 0,
-    estado: "Colaborador",
-  },
-  {
-    nombre: "Delfina Mora",
-    username: "@delfinamora",
-    actividad: "No hay actividad reciente",
-    tableros: 0,
-    estado: "Colaborador",
-  },
-  {
-    nombre: "Juan Ignacio Mora",
-    username: "@juanignaciomora",
-    actividad: "No hay actividad reciente",
-    tableros: 0,
-    estado: "Colaborador",
-  },
-];
+import { useSelector } from "react-redux";
 
 export function MiembrosPage() {
+  const usuarios = useSelector((state) => state.miembro.miembros);
   const [openModalInvitar, setOpenModalInvitar] = useState(false);
   const [openModalQuitar, setOpenModalQuitar] = useState(false);
   const onCloseModalInvitar = () => {
