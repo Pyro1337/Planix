8a4a3c8f04077fc5f9993a12a2824cea5c938712
lucasgoncalvez <goncalvezlucas281@gmail.com>
2024-12-11 11:09:31 -0300

    fix: en dashboard muestra Desconocido en vez de null

--- trello-frontend/src/modules/reporte/pages/ReporteTableroPage.js
+++ trello-frontend/src/modules/reporte/pages/ReporteTableroPage.js
@@ -24,18 +24,21 @@ export function ReporteTableroPage() {
     Object.values(tableros).forEach((columna) => {
       columna.items.forEach((item) => {
         // Incrementa el conteo de items para cada usuario
-        if (userItemCounts[item.user]) {
-          userItemCounts[item.user]++;
+        if (userItemCounts[item.user || "Desconocido"]) {
+          userItemCounts[item.user || "Desconocido"]++;
         } else {
-          userItemCounts[item.user] = 1;
+          userItemCounts[item.user || "Desconocido"] = 1;
         }
+        console.log(userItemCounts, item);
       });
     });
     // Convierte el objeto en un arreglo en el formato solicitado
-    return Object.entries(userItemCounts).map(([name, value]) => ({
+    const data = Object.entries(userItemCounts).map(([name, value]) => ({
       name,
       value,
     }));
+    console.log("data", data);
+    return data;
   };
 
   return (
